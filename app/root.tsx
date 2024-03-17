5d9cba89f6b1c3c6348aaca7e918bb008e7665a9
Tracy <zhangtracy03@gmail.com>
2024-03-16 21:24:19 -0700

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,165 @@
+import type {
+  LinksFunction,
+  LoaderFunctionArgs,
+} from "@remix-run/node";
+
+import { useEffect, useState } from "react";
+//import { useEffect } from "react";
+
+//import type { LinksFunction } from "@remix-run/node";
+// existing imports
+
+import appStylesHref from "./app.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+
+import { json, redirect } from "@remix-run/node";
+
+
+import {
+  Form,
+  Links,
+  LiveReload,
+  Meta,
+  NavLink,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "@remix-run/react";
+
+//import { getContacts } from "./data";
+
+import { createEmptyContact, getContacts } from "./data";
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};
+
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
+};
+
+export default function App() {
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const submit = useSubmit();
+  const searching =
+  navigation.location &&
+  new URLSearchParams(navigation.location.search).has(
+    "q"
+  );
+  // the query now needs to be kept in state
+  const [query, setQuery] = useState(q || "");
+
+  // we still have a `useEffect` to synchronize the query
+  // to the component state on back/forward button clicks
+  useEffect(() => {
+    setQuery(q || "");
+  }, [q]);
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+    <body>
+      <div id="sidebar">{<h1>Remix Contacts</h1>}<div>
+      <Form
+              id="search-form"
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
+              role="search"
+            >
+              <input
+                aria-label="Search contacts"
+                className={searching ? "loading" : ""}
+                defaultValue={q || ""}
+                id="q"
+                name="q"
+                placeholder="Search"
+                type="search"
+              />
+              <div
+                aria-hidden
+                hidden={!searching}
+                id="search-spinner"
+              />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            {contacts.length ? (
+              <ul>
+              {contacts.map((contact) => (
+              <li key={contact.id}>
+                <NavLink
+                className={({ isActive, isPending }) =>
+                isActive
+                  ? "active"
+                  : isPending
+                  ? "pending"
+                  : ""
+              }
+              to={`contacts/${contact.id}`}
+                >
+
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </nav>
+        </div>
+
+        <div
+          className={
+            navigation.state === "loading" && !searching
+              ? "loading"
+              : ""
+          }
+          id="detail"
+        >
+          <Outlet />
+        </div>
+
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
